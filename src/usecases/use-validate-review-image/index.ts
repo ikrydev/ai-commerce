c93dd4b49f2a1ef0a4e783c23c4b8644b167835b
agus.suyuthi <agus.suyuthi@bytedance.com>
2025-03-14 15:53:22 +0700

    feat: improve logic image review validation

--- src/usecases/use-validate-review-image/index.ts
+++ src/usecases/use-validate-review-image/index.ts
@@ -3,23 +3,40 @@ import useImageToText from '../../hooks/use-image-to-text';
 const useValidateReviewImage = () => {
   const { session, isEligible } = useImageToText();
 
-  const handleValidate = async (
-    title: string,
-    imgReview: File | Blob,
-    onSuccess: () => void,
-  ) => {
+  const handleValidate = async ({
+    title,
+    img,
+    onSuccess,
+  }: {
+    title: string;
+    img: File | Blob;
+    onSuccess: ({ score, reason }: { score: number; reason: string }) => void;
+  }) => {
     try {
       if (isEligible && session) {
         try {
-          const imageBitmap = await createImageBitmap(imgReview);
+          let jsonResult = { score: 0, reason: 'error' };
+          const imageBitmap = await createImageBitmap(img);
 
           // @ts-ignore
           const result = await session?.prompt([
-            `Analyze the image provided. what inside the image and check is the image relevant with the product with title "${title}. answer with percentegae 1-100%. avoid compare color of product and answer with this template valid JSON { score: "", reason: "" }"`,
+            `Analyze the image provided. what inside the image and check is the image stricly relevant with the product with title "${title}. answer with percentegae 1-100%. avoid compare color of product and answer with object valid JSON only : { score: "", reason: "" }"`,
             { type: 'image', content: imageBitmap },
           ]);
 
+          try {
+            jsonResult = JSON.parse(result);
+          } catch (error) {
+            jsonResult = { score: 0, reason: 'error' };
+          }
+
           console.log('Result: ' + result);
+
+          typeof onSuccess === 'function' &&
+            onSuccess({
+              score: Number.isNaN(jsonResult.score) ? 0 : Number(jsonResult.score),
+              reason: jsonResult.reason || '',
+            });
 
           return result;
         } catch (bitmapError) {
